372b5ed5edc850ab2b458094e4b56d44d34abf45
Mohammad Mahdi Nemati Haravani <nemati.mahdi255@gmail.com>
2023-07-31 06:31:28 +0330

    finish and test CRC

--- Module/CRC.vhd
+++ Module/CRC.vhd
@@ -4,33 +4,35 @@ use ieee.numeric_std.all;
 use work.CRC_Package.all;
 
 entity CRC is
+	generic 
+	(
+		Data_W : integer := 8;
+		CRC_W : integer := 16
+	);
 	port
 	(
 		i_Rst_n  : in  std_logic;
 		i_Clk    : in  std_logic;
 		i_En     : in  std_logic;
-		i_RD     : in  std_logic;
 
-		i_Poly   : in  std_logic_vector (7 downto 0);
-		i_Init   : in  std_logic_vector (7 downto 0);
-		i_XorOut : in  std_logic_vector (7 downto 0);
+		i_Poly   : in  std_logic_vector (CRC_W - 1 downto 0);
+		i_Init   : in  std_logic_vector (CRC_W - 1 downto 0);
+		i_XorOut : in  std_logic_vector (CRC_W - 1 downto 0);
 		i_RefIn  : in  std_logic;
 		i_RefOut : in  std_logic;
 
-		i_Byte   : in  std_logic_vector (7 downto 0);
-		o_Valid  : out std_logic;
-		o_CRC    : out std_logic_vector (7 downto 0)
+		i_Byte   : in  std_logic_vector (Data_W - 1 downto 0);
+		o_CRC    : out std_logic_vector (CRC_W - 1 downto 0)
 	);
 end CRC;
 
 architecture RTL of CRC is
 
-	type crc_array is array(natural range <>) of std_logic_vector(7 downto 0);
+	type crc_array is array(natural range <>) of std_logic_vector(CRC_W - 1 downto 0);
 
-	signal r_CRC   : std_logic_vector (7 downto 0);
-	signal w_CRC   : crc_array (7 downto 0);
-	signal w_Byte  : std_logic_vector (7 downto 0);
-	signal r_Valid : std_logic;
+	signal r_CRC   : std_logic_vector (CRC_W - 1 downto 0);
+	signal w_CRC   : crc_array (Data_W - 1 downto 0);
+	signal w_Byte  : std_logic_vector (Data_W - 1 downto 0);
 begin
 
 	process (i_Rst_n, i_Clk, i_Init)
@@ -38,15 +40,9 @@ begin
 	
 		if i_Rst_n = '0' then
 			r_CRC   <= i_Init;
-			r_Valid    <= '0';
 			
-		elsif rising_edge(i_Clk) and i_En = '1' and r_Valid = '0' then
-		
-			if i_RD = '0' then
-				r_Valid <= '1';
-			end if;
-			
-			r_CRC <= w_CRC(7);
+		elsif rising_edge(i_Clk) and i_En = '1' then
+			r_CRC <= w_CRC(Data_W - 1);
 
 		end if;
 		
@@ -55,27 +51,25 @@ begin
 	process (r_CRC, w_CRC, w_Byte, i_Poly)
 	begin
 	
-		if r_CRC(7) = '0' then
-			w_CRC(0) <= r_CRC(6 downto 0) & w_Byte(0);
+		if r_CRC(CRC_W - 1) = '0' then
+			w_CRC(0) <= r_CRC(CRC_W - 2 downto 0) & w_Byte(0);
 		else
-			w_CRC(0) <= (r_CRC(6 downto 0) & w_Byte(0)) xor i_Poly;
+			w_CRC(0) <= (r_CRC(CRC_W - 2 downto 0) & w_Byte(0)) xor i_Poly;
 		end if;
 		
-		for i in 1 to 7 loop -- loop over rows
-			if w_CRC(i-1)(7) = '0' then
-				w_CRC(i) <= w_CRC(i-1)(6 downto 0) & w_Byte(i);
+		for i in 1 to Data_W - 1 loop -- loop over rows
+			if w_CRC(i-1)(CRC_W - 1) = '0' then
+				w_CRC(i) <= w_CRC(i-1)(CRC_W - 2 downto 0) & w_Byte(i);
 			else
-				w_CRC(i) <= (w_CRC(i-1)(6 downto 0) & w_Byte(i)) xor i_Poly;
+				w_CRC(i) <= (w_CRC(i-1)(CRC_W - 2 downto 0) & w_Byte(i)) xor i_Poly;
 			end if;
 		end loop;
 		
 	end process;
 	
-	w_Byte <= (others => '0') when i_RD = '0' else
-				i_Byte when i_RefIn = '1' else
+	w_Byte <= i_Byte when i_RefIn = '1' else
 				reverse(i_Byte);
 	o_CRC <= r_CRC xor i_XorOut when i_RefOut = '0' else
 			reverse(r_CRC xor i_XorOut);
-	o_valid <= r_Valid;
 
 end RTL;
--- Test Bench/CRC_TB.vhd
+++ Test Bench/CRC_TB.vhd
@@ -15,15 +15,13 @@ architecture behavior of CRC_TB is
 			i_Rst_n  : in  std_logic;
 			i_Clk    : in  std_logic;
 			i_En     : in  std_logic;
-			i_RD     : in  std_logic;
-			i_Poly   : in  std_logic_vector(7 downto 0);
-			i_Init   : in  std_logic_vector(7 downto 0);
-			i_XorOut : in  std_logic_vector(7 downto 0);
+			i_Poly   : in  std_logic_vector(15 downto 0);
+			i_Init   : in  std_logic_vector(15 downto 0);
+			i_XorOut : in  std_logic_vector(15 downto 0);
 			i_RefIn  : in  std_logic;
 			i_RefOut : in  std_logic;
 			i_Byte   : in  std_logic_vector(7 downto 0);
-			o_Valid  : out std_logic;
-			o_CRC    : out std_logic_vector(7 downto 0)
+			o_CRC    : out std_logic_vector(15 downto 0)
 		);
 	end component;
 
@@ -31,17 +29,15 @@ architecture behavior of CRC_TB is
 	signal i_Rst_n        : std_logic                    := '0';
 	signal i_Clk          : std_logic                    := '0';
 	signal i_En           : std_logic                    := '0';
-	signal i_RD           : std_logic                    := '0';
-	signal i_Poly         : std_logic_vector(7 downto 0) := (others => '0');
-	signal i_Init         : std_logic_vector(7 downto 0) := (others => '0');
-	signal i_XorOut       : std_logic_vector(7 downto 0) := (others => '0');
+	signal i_Poly         : std_logic_vector(15 downto 0) := (others => '0');
+	signal i_Init         : std_logic_vector(15 downto 0) := (others => '0');
+	signal i_XorOut       : std_logic_vector(15 downto 0) := (others => '0');
 	signal i_RefIn        : std_logic                    := '0';
 	signal i_RefOut       : std_logic                    := '0';
 	signal i_Byte         : std_logic_vector(7 downto 0) := (others => '0');
 
 	--Outputs
-	signal o_Valid        : std_logic;
-	signal o_CRC          : std_logic_vector(7 downto 0);
+	signal o_CRC          : std_logic_vector(15 downto 0);
 
 	-- Clock period definitions
 	constant i_Clk_period : time := 10 ns;
@@ -54,14 +50,12 @@ begin
 		i_Rst_n  => i_Rst_n,
 		i_Clk    => i_Clk,
 		i_En     => i_En,
-		i_RD     => i_RD,
 		i_Poly   => i_Poly,
 		i_Init   => i_Init,
 		i_XorOut => i_XorOut,
 		i_RefIn  => i_RefIn,
 		i_RefOut => i_RefOut,
 		i_Byte   => i_Byte,
-		o_Valid  => o_Valid,
 		o_CRC    => o_CRC
 	);
 
@@ -80,24 +74,25 @@ begin
 		-- hold reset state for 100 ns.
 		i_Rst_n <= '0';
 		i_En <= '0';
-		i_RD <= '0';
-		i_Poly <= x"07";
-		i_Init <= x"00";
-		i_XorOut <= x"00";
+		i_Poly <= x"1021";
+		i_Init <= x"0000";
+		i_XorOut <= x"0000";
 		i_RefIn <= '0';
 		i_RefOut <= '0';
-		i_Byte <= x"FF";
 		wait for i_Clk_period * 10;
 		
 		i_Rst_n <= '1';
+		
 		i_En <= '1';
-		i_RD <= '1';
+		i_Byte <= x"FF";
 		wait for i_Clk_period;
 		i_En <= '1';
-		i_RD <= '0';
+		i_Byte <= x"00";
+		wait for i_Clk_period;
+		i_Byte <= x"00";
+		i_En <= '1';
 		wait for i_Clk_period;
 		i_En <= '0';
-		i_RD <= '0';
 		-- insert stimulus here 
 
 		wait;
